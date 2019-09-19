5e7271045c5cd736e3aeb2d58ec608056df35fa9
Nguyen Huu Phuong <phuong.nguyenhuucse@gmail.com>
2019-09-19 11:09:16 +0700

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,11 +2,11 @@
 
 TRY_LOOP="20"
 
-: "${REDIS_HOST:="redis"}"
+: "${REDIS_HOST:="redis_host"}"
 : "${REDIS_PORT:="6379"}"
 : "${REDIS_PASSWORD:=""}"
 
-: "${POSTGRES_HOST:="postgres"}"
+: "${POSTGRES_HOST:="postgres_host"}"
 : "${POSTGRES_PORT:="5432"}"
 : "${POSTGRES_USER:="airflow"}"
 : "${POSTGRES_PASSWORD:="airflow"}"
